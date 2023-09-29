2cf5610a22690ff26623b6bad0ba57b856128f84
Adil Choudhury <a.choudhury4927@gmail.com>
2023-09-29 21:31:32 +0100

    chore: created deploy script file DeployDecentralisedStableCoin.sol

---
+++ script/DeployDecentralisedStableCoin.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {DecentralisedStableCoin} from "../src/DecentralisedStableCoin.sol";
+
+/**
+ * @title DeployDecentralisedStableCoin
+ * @author Adil Choudhury
+ *
+ * This contract will deploy ASC token contract DecentralisedStableCoin
+ */
+contract DeployDecentralisedStableCoin {
+    DecentralisedStableCoin ascContract;
+}
