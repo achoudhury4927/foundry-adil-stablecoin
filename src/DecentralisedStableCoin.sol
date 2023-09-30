4af9cbc501195bf758a023c715396c39f83f3c89
Adil Choudhury <a.choudhury4927@gmail.com>
2023-09-30 10:36:54 +0100

    chore: set solc version to 0.8.21 and no ranges as security consideration

--- src/DecentralisedStableCoin.sol
+++ src/DecentralisedStableCoin.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.18;
+pragma solidity 0.8.21;
 
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
