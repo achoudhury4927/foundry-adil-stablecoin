d16735cb4321ff2c7622e816d70f1c1a70d75884
Adil Choudhury <a.choudhury4927@gmail.com>
2023-09-30 10:33:22 +0100

    feat: added burn and mint functions to DecentralisedStableCoin.sol

--- src/DecentralisedStableCoin.sol
+++ src/DecentralisedStableCoin.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.18;
 
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title DecentralisedStableCoin
@@ -13,6 +14,39 @@ import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensio
  *
  * This contract is meant to governed by DSCEngine.sol. This contract is an ERC20 implementation of our stablecoin system.
  */
-contract DecentralisedStableCoin is ERC20Burnable {
+contract DecentralisedStableCoin is ERC20Burnable, Ownable {
+    error DecentralisedStableCoin__AmountMustBeMoreThanZero();
+    error DecentralisedStableCoin__BurnAmountExceedsBalance();
+    error DecentralisedStableCoin__NotZeroAddress();
+
     constructor() ERC20("DecentralisedStableCoin", "ASC") {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount < 0) {
+            revert DecentralisedStableCoin__AmountMustBeMoreThanZero();
+        }
+        //Question: Is this required? Line 283 in ERC20.sol has this same require
+        //TODO: Test with this code commented out and check behaviour
+        if (balance < _amount) {
+            revert DecentralisedStableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        //Question: Is this required? Line 252 in ERC20.sol has this same require
+        //TODO: Test with this code commented out and check behaviour
+        if (_to == address(0)) {
+            revert DecentralisedStableCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralisedStableCoin__AmountMustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
 }
